bbaa108bf7bd7548ec858438c3a7199d5171c1b3
singhpriti <singhpriti281@gmail.com>
2024-02-23 00:17:58 +0530

    Added Autolayout

--- UserHoldingScreen/Screen/HoldingScreen/View/Controller/HoldingListViewController.swift
+++ UserHoldingScreen/Screen/HoldingScreen/View/Controller/HoldingListViewController.swift
@@ -13,27 +13,12 @@ class HoldingListViewController: UIViewController {
     @IBOutlet weak var PortfolioTableView : UITableView!
     
     var model = HoldingsViewModel()
-    let portfolioView = PortfolioView()
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
         config()
-        view.addSubview(portfolioView)
     }
-    
-//    override func viewDidLayoutSubviews() {
-//         super.viewDidLayoutSubviews()
-//         
-//         // Calculate the total height of the table view content
-//         var contentHeight: CGFloat = 0
-//         for section in 0..<HoldingTableView.numberOfSections {
-//             contentHeight += HoldingTableView.rect(forSection: section).height
-//         }
-//         
-//         // Set the frame of the PortfolioView below the last cell
-//         let portfolioViewY = contentHeight + HoldingTableView.contentInset.top
-//         portfolioView.frame = CGRect(x: 0, y: portfolioViewY, width: view.bounds.width, height: 200)
-//     }
  }
 
 
@@ -42,6 +27,8 @@ extension HoldingListViewController {
     
     func config(){
         HoldingTableView.register(UINib(nibName: "HoldingsCellTableViewCell" , bundle: nil), forCellReuseIdentifier: "holdingCell")
+        
+        PortfolioTableView.register(UINib(nibName: "PortfolioViewTableViewCell", bundle: nil), forCellReuseIdentifier: "PortfolioCell")
         viewModelInit()
         navigationController?.navigationBar.prefersLargeTitles = false
         
@@ -68,6 +55,7 @@ extension HoldingListViewController {
             case.dataLoaded:
                 DispatchQueue.main.async {
                     self.HoldingTableView.reloadData()
+                    self.PortfolioTableView.reloadData()
                 }
             case .error(let error):
                 print(error)
@@ -77,18 +65,67 @@ extension HoldingListViewController {
     }
 }
 
-extension HoldingListViewController : UITableViewDataSource{
+extension HoldingListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        model.holdings.count
+        if tableView == HoldingTableView {
+            return model.holdings.count
+        } else if tableView == PortfolioTableView {
+    
+            return 1
+        }
+        return 0
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+          if tableView == PortfolioTableView {
+              return 300
+          }
+          return UITableView.automaticDimension
+      }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "holdingCell", for: indexPath) as? HoldingsCellTableViewCell  else { return UITableViewCell() }
-        let data =  model.holdings[indexPath.row]
-        // assigning the data to cell.data
-        cell.data = data
-        return cell
+        if tableView == HoldingTableView {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "holdingCell", for: indexPath) as? HoldingsCellTableViewCell else {
+                return UITableViewCell()
+            }
+            let data =  model.holdings[indexPath.row]
+            cell.data = data
+            return cell
+        } else if tableView == PortfolioTableView {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "PortfolioCell", for: indexPath) as? PortfolioViewTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            // Calculate aggregated data for the portfolio
+    
+              let totalCurrentValue = model.holdings.reduce(0.0) { $0 + $1.currentValue }
+              let totalInvestmentValue = model.holdings.reduce(0.0) { $0 + $1.investmentValue }
+              let totalPnl = totalCurrentValue - totalInvestmentValue
+              let totalTodayPnl = model.holdings.reduce(0.0) { $0 + $1.todayPnl }
+            
+            let formattedCurrentValue = String(format: "%.2f", totalCurrentValue)
+            let formattedInvestmentValue = String(format: "%.2f", totalInvestmentValue)
+            let formattedPnl = String(format: "%.2f", totalPnl)
+            let formattedTodayPnl = String(format: "%.2f", totalTodayPnl)
+            
+            
+            cell.CurrentValue.text = "Current Value"
+            cell.TotalInvestment.text = "Total Investment"
+            cell.TodayPandL.text = "Today's Profit & Loss"
+            cell.OverAllPandL.text = "Profit & Loss"
+            
+            cell.CurrentAmount.text = "\u{20B9}\(formattedCurrentValue)"
+            cell.TotalInvestmentAmount.text = "\u{20B9}\(formattedInvestmentValue)"
+            cell.TodaysPandLAmount.text = "\u{20B9}\(formattedTodayPnl)"
+            cell.OverallAmount.text = "\u{20B9}\(formattedPnl)"
+            
+
+            return cell
+
+        }
+        return UITableViewCell()
     }
-    
-    
 }
+
+
+
